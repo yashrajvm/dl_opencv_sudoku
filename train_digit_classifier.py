22d10dcc4cb3fc5fc9f41a2e763bd2d51c359b5c
yashrajvm <yashrajvumolawade@gmail.com>
2020-08-12 18:38:28 +0530

    trained model

--- train_digit_classifier.py
+++ train_digit_classifier.py
@@ -1,7 +1,7 @@
-# USAGE
+
 # python train_digit_classifier.py --model output/digit_classifier.h5
 
-# import the necessary packages
+
 from pyimagesearch.models import SudokuNet
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.datasets import mnist
@@ -9,43 +9,42 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 import argparse
 
-# construct the argument parser and parse the arguments
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", required=True,
 	help="path to output model after training")
 args = vars(ap.parse_args())
 
-# initialize the initial learning rate, number of epochs to train
-# for, and batch size
+
 INIT_LR = 1e-3
 EPOCHS = 10
 BS = 128
 
-# grab the MNIST dataset
+
 print("[INFO] accessing MNIST...")
 ((trainData, trainLabels), (testData, testLabels)) = mnist.load_data()
 
-# add a channel (i.e., grayscale) dimension to the digits
+
 trainData = trainData.reshape((trainData.shape[0], 28, 28, 1))
 testData = testData.reshape((testData.shape[0], 28, 28, 1))
 
-# scale data to the range of [0, 1]
+
 trainData = trainData.astype("float32") / 255.0
 testData = testData.astype("float32") / 255.0
 
-# convert the labels from integers to vectors
+
 le = LabelBinarizer()
 trainLabels = le.fit_transform(trainLabels)
 testLabels = le.transform(testLabels)
 
-# initialize the optimizer and model
+
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR)
 model = SudokuNet.build(width=28, height=28, depth=1, classes=10)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# train the network
+
 print("[INFO] training network...")
 H = model.fit(
 	trainData, trainLabels,
@@ -54,7 +53,7 @@ H = model.fit(
 	epochs=EPOCHS,
 	verbose=1)
 
-# evaluate the network
+
 print("[INFO] evaluating network...")
 predictions = model.predict(testData)
 print(classification_report(
@@ -62,6 +61,6 @@ print(classification_report(
 	predictions.argmax(axis=1),
 	target_names=[str(x) for x in le.classes_]))
 
-# serialize the model to disk
+
 print("[INFO] serializing digit model...")
 model.save(args["model"], save_format="h5")
